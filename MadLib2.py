67cf2757e8fd6adf13eb288d5c0c6757bac39366
Joshua Bothell <joshua.bothell@tooeleschools.org>
2018-09-07 09:20:13 -0600

    add unit one examples

---
+++ MadLib2.py
@@ -0,0 +1,10 @@
+word1 = input("ENTER A ADJECTIVE... ")
+word2 = input("ENTER A NOUN... ")
+word3 = input("ENTER A ANIMAL(OR NOUN)... ")
+word4 = input("ENTER A NOISE... ")
+
+print("""{} Macdonald had a {},
+E-I-E-I-O and on that {} he had an {}, E-I-E-I-O
+with a {} {} here and a {} {} there, here
+a {}, there a {}, everywhere a {} {}
+{} Macdonald had a {}, E-I-E-I-O""".format(word1, word2, word2, word3, word4, word4, word4, word4, word4, word4, word4, word4, word1, word2))
---
+++ newidle.py
@@ -0,0 +1,6 @@
+print("this is a string")
+name = "Josh"
+print(type(name))
+fact = "I like tyson vorwaller"
+message = "my name is " + name + " and " + fact
+print(message)
